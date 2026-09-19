8c8164a27f9a513cd21c15c83a8c10e1dc159797
MaverickdUMB <f20251379@pilani.bits-pilani.ac.in>
2026-09-19 16:25:42 +0000

    lab02 task2

--- labs/lab02/task2/lut.v
+++ labs/lab02/task2/lut.v
@@ -16,11 +16,18 @@ module lut #(
 
   integer i;
 
+initial begin
+    for (i = 0; i < DEPTH; i = i + 1) begin
+        mem[i] = i * i;
+    end
+end
   // TODO: initialize mem[i] = i*i for every i from 0 to DEPTH-1.
   // Use an initial block with a for loop -- this is the only place a ROM's
   // contents should be set up. (See the lab manual for why.)
   
-
+always @(*) begin
+    dout = mem[sel];
+end
   // TODO: make dout continuously reflect mem[sel]. This is a combinational
   // read -- pick the right procedural block and sensitivity list.
 
